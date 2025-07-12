266e9021154956bf1dcf22b4f04e7e1d7445c5f7
ODOO2025-HACKATHON <vinayvakdaka1608@gmail.com>
2025-07-12 11:50:57 +0530

    created Regitser and fronted setup

--- Client/src/Context/AppState.jsx
+++ Client/src/Context/AppState.jsx
@@ -4,9 +4,19 @@ import AppContext from "./AppContext";
 import React, { useState } from 'react'
 
 const AppState = (props) => {
-   const [a, setA] = useState("Hello World");
+  const url = 'http://localhost:3000/api';
+   const register = async (name, email, password) => {
+
+        const api = await axios.post(`${url}/user/register`, { name, email, password }, {
+            headers: {
+                "Content-Type": "Application/json"
+            }, withCredentials: true
+        });
+      }
+  
+   const [a, setA] = useState("Lord Suhas ");
   return (
-    <AppContext.Provider value={{a}}>{props.children}</AppContext.Provider>
+    <AppContext.Provider value={{register}}>{props.children}</AppContext.Provider>
       
   
   )
